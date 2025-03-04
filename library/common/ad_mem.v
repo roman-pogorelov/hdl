68db57eee90d314fc4469de5e1ec148c25ffffc0
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2025-03-04 13:02:12 +0300

    Replace the implementation of a simple dual port RAM from generic to Altera/Intel specific

--- library/common/ad_mem.v
+++ library/common/ad_mem.v
@@ -1,71 +1,69 @@
-// ***************************************************************************
-// ***************************************************************************
-// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
-//
-// In this HDL repository, there are many different and unique modules, consisting
-// of various HDL (Verilog or VHDL) components. The individual modules are
-// developed independently, and may be accompanied by separate and unique license
-// terms.
-//
-// The user should read each of these license terms, and understand the
-// freedoms and responsibilities that he or she has by using this source/core.
-//
-// This core is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE.
-//
-// Redistribution and use of source or resulting binaries, with or without modification
-// of this file, are permitted under one of the following two license terms:
-//
-//   1. The GNU General Public License version 2 as published by the
-//      Free Software Foundation, which can be found in the top level directory
-//      of this repository (LICENSE_GPL2), and also online at:
-//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
-//
-// OR
-//
-//   2. An ADI specific BSD license, which can be found in the top level directory
-//      of this repository (LICENSE_ADIBSD), and also on-line at:
-//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
-//      This will allow to generate bit files and not release the source code,
-//      as long as it attaches to an ADI device.
-//
-// ***************************************************************************
-// ***************************************************************************
+module ad_mem
+#(
+    parameter                           DATA_WIDTH      = 16,
+    parameter                           ADDRESS_WIDTH   = 5
+)
+(
+    input                               clka,
+    input                               wea,
+    input  [(ADDRESS_WIDTH - 1) : 0]    addra,
+    input  [(DATA_WIDTH - 1) : 0]       dina,
 
-`timescale 1ns/100ps
-
-module ad_mem #(
-
-  parameter  DATA_WIDTH = 16,
-  parameter  ADDRESS_WIDTH = 5) (
-
-  input                               clka,
-  input                               wea,
-  input       [(ADDRESS_WIDTH-1):0]   addra,
-  input       [(DATA_WIDTH-1):0]      dina,
-
-  input                               clkb,
-  input                               reb,
-  input       [(ADDRESS_WIDTH-1):0]   addrb,
-  output  reg [(DATA_WIDTH-1):0]      doutb);
-
-  (* ram_style = "block" *)
-  reg         [(DATA_WIDTH-1):0]      m_ram[0:((2**ADDRESS_WIDTH)-1)];
-
-  always @(posedge clka) begin
-    if (wea == 1'b1) begin
-      m_ram[addra] <= dina;
-    end
-  end
-
-  always @(posedge clkb) begin
-    if (reb == 1'b1) begin
-      doutb <= m_ram[addrb];
-    end
-  end
-
+    input                               clkb,
+    input                               reb,
+    input  [(ADDRESS_WIDTH - 1) : 0]    addrb,
+    output [(DATA_WIDTH - 1) : 0]       doutb
+);
+    altera_syncram #(
+        .address_aclr_b         ("NONE"),
+        .address_reg_b          ("CLOCK1"),
+        .clock_enable_input_a   ("BYPASS"),
+        .clock_enable_input_b   ("BYPASS"),
+        .clock_enable_output_b  ("BYPASS"),
+        .intended_device_family ("Arria 10"),
+        .lpm_type               ("altera_syncram"),
+        .numwords_a             (2**ADDRESS_WIDTH),
+        .numwords_b             (2**ADDRESS_WIDTH),
+        .operation_mode         ("DUAL_PORT"),
+        .outdata_aclr_b         ("NONE"),
+        .outdata_sclr_b         ("NONE"),
+        .outdata_reg_b          ("UNREGISTERED"),
+        .power_up_uninitialized ("FALSE"),
+        .rdcontrol_reg_b        ("CLOCK1"),
+        .widthad_a              (ADDRESS_WIDTH),
+        .widthad_b              (ADDRESS_WIDTH),
+        .width_a                (DATA_WIDTH),
+        .width_b                (DATA_WIDTH),
+        .width_byteena_a        (1)
+    )
+    altera_syncram_component (
+        .address_a              (addra),
+        .address_b              (addrb),
+        .clock0                 (clka),
+        .clock1                 (clkb),
+        .data_a                 (dina),
+        .rden_b                 (reb),
+        .wren_a                 (wea),
+        .q_b                    (doutb),
+        .aclr0                  (1'b0),
+        .aclr1                  (1'b0),
+        .address2_a             (1'b1),
+        .address2_b             (1'b1),
+        .addressstall_a         (1'b0),
+        .addressstall_b         (1'b0),
+        .byteena_a              (1'b1),
+        .byteena_b              (1'b1),
+        .clocken0               (1'b1),
+        .clocken1               (1'b1),
+        .clocken2               (1'b1),
+        .clocken3               (1'b1),
+        .data_b                 ({DATA_WIDTH{1'b1}}),
+        .eccencbypass           (1'b0),
+        .eccencparity           (8'b0),
+        .eccstatus              (),
+        .q_a                    (),
+        .rden_a                 (1'b1),
+        .sclr                   (1'b0),
+        .wren_b                 (1'b0)
+    ); // altera_syncram_component
 endmodule
-
-// ***************************************************************************
-// ***************************************************************************
